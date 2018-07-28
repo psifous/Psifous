95186647db481be894f27c81b973bc3e98969b0a
Dwiky Erlangga <dwikyerl@gmail.com>
2018-07-28 13:58:26 +0700

    Create Dashboard Page for admin

--- server/routes/communities.js
+++ server/routes/communities.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const {createCommunity,getCommunityData,deleteData,updateData,getOneCommunity} = require('../controller/communityController')
+const {
+  createCommunity,
+  getCommunityData,
+  deleteData,
+  updateData,
+  getOneCommunity
+} = require('../controller/communityController');
 
-router.post('/',createCommunity)
-router.get('/',getCommunityData);
-router.get('/:id',getOneCommunity)
-router.put('/:id',updateData)
-router.delete('/:id',deleteData)
-
+router.post('/', createCommunity);
+router.get('/', getCommunityData);
+router.get('/:id', getOneCommunity);
+router.put('/:id', updateData);
+router.delete('/:id', deleteData);
 
 module.exports = router;
