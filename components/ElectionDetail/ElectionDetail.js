6274ee756c14d1e16122c593e7703882c30f35ae
annisaayu <annisaayu569@gmail.com>
2018-07-29 11:37:27 +0700

    component election detail

---
+++ components/ElectionDetail/ElectionDetail.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Button, Checkbox, Form, Segment, Grid, Card, Icon, Image } from 'semantic-ui-react';
+import Link from 'next/link';
+
+
+class ElectionPage extends React.Component {
+
+  render() {
+    return (
+      <Grid columns={2} padded='vertically'>
+        <Grid.Column>
+          <Card>
+            <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' />
+            <Card.Content>
+              <Card.Header>Matthew</Card.Header>
+              <Card.Meta>
+                <span className='date'>Joined in 2015</span>
+              </Card.Meta>
+              <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                22 Friends
+              </a>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+        <Grid.Column>
+          <Card>
+            <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' />
+            <Card.Content>
+              <Card.Header>Matthew</Card.Header>
+              <Card.Meta>
+                <span className='date'>Joined in 2015</span>
+              </Card.Meta>
+              <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                22 Friends
+              </a>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+        <Grid.Column width={8}>
+          <Link href="election/vote-booth">
+            <Button
+                id="vote-button"
+                icon="check"
+                color="green"
+                content="Vote Candidate"
+              />
+          </Link>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+const mapStateToprops = state => {
+  return {
+    isLogin: state.auth
+  }
+}
+export default connect(mapStateToprops, null)(ElectionPage);
