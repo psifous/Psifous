e5520f8222d3c5f26c44aa84e7d92662ef54cf0a
Dwiky Erlangga <dwikyerl@gmail.com>
2018-08-01 17:39:30 +0700

    Delete websocket

--- ethereum/web3socket.js
+++
@@ -1,9 +0,0 @@
-import Web3 from 'web3';
-
-const provider = new Web3.providers.WebsocketProvider(
-  'wss://rinkeby.infura.io/ws'
-);
-
-const web3Socket = new Web3(provider);
-
-export default web3Socket;
